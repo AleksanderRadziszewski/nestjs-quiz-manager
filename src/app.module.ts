4949b5ed148896acaef7585d14159fdf1d83b823
AleksanderRadziszewski <radziszewski.aleksander@gmail.com>
2022-10-11 12:00:15 +0200

    config module, database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { QuizModule } from './modules/quiz/quiz.module';
-import { join } from 'path';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [join(__dirname, '**/*.entity.js')],
-        synchronize: true,
-        autoLoadEntities: true,
-      }),
-    }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     QuizModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
