0e2d71c1d64510cf39adc15f8aa1fc29ee233add
AleksanderRadziszewski <radziszewski.aleksander@gmail.com>
2022-10-13 15:11:07 +0200

    local strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,22 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { QuizModule } from './modules/quiz/quiz.module';
 import { typeOrmConfigAsync } from './config/typeorm.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserController } from './modules/user/user.controller';
+import { UserModule } from './modules/user/user.module';
+import { UserService } from './modules/user/user.service';
+import { UserModule } from './modules/user/user.module';
+import { UserController } from './modules/user/user.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     QuizModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
